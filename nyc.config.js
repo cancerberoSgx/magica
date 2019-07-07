022c9116aa56d3c21598ece3e8ede292d11ba8b7
Sebastián Gurín <sgurin@awalabs.com>
2019-07-07 10:32:51 -0300

    coveral

--- nyc.config.js
+++ nyc.config.js
@@ -1,9 +1,7 @@
-module.exports =  {
+module.exports = {
   "extends": "@istanbuljs/nyc-config-typescript",
-  // "all": true,
-  "exclude": ['test-browser-outdir',"dist", "src/imageMagick/compiled" ],
-    "reporter": [
-      "html", 'lcov', 'text-summary'
-    ],
-  // "check-coverage": true
+  "exclude": ['test-browser-outdir', "dist", "src/imageMagick/compiled", "test"],
+  "reporter": [
+    "html", 'lcov', 'text-summary'
+  ],
 }
