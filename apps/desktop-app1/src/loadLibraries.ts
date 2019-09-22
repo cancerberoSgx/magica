3c7348fcc82a3be1e3ed2cfb7dfc75de3f729f98
Sebastián Gurín <sgurin@awalabs.com>
2019-09-22 05:53:07 -0300

    ci

--- apps/desktop-app1/src/loadLibraries.ts
+++ apps/desktop-app1/src/loadLibraries.ts
@@ -3,15 +3,7 @@ import { magickLoaded } from 'magica'
 export async function loadLibraries() {
   try {
     await magickLoaded
-    // console.log(await run({script: 'identify rose:'}), 'se');
-    // const Magica = {
-    //   fromArrayBuffer: MagicaFile.fromArrayBuffer,
-    //   fromRGBAImageData: async (data: ImageData) => MagicaFile.fromRGBAImageData(data as any),
-    //   run
-    // }
-    // await loadOpencv({ formatProxies: [() => new MagicaCodec(Magica)] })
   } catch (error) {
     console.error(error)
-
   }
 }
